aea8aaaa2145052299935fc9e5d0eb07d1fc087e
Flarp <>
2019-10-03 20:03:57 -0400

    AAAAAa

---
+++ index.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js")
+const client = new Discord.Client()
+const HIM = "257341876628226059"
+
+client.on("message", message => {
+    if (message === "Start" && message.author.id === HIM) {
+        console.log("this is the end")
+    }
+}
+
+
